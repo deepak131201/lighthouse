713940ed18afd0764dc525eee63c92f948400d23
Connor Clark <cjamcl@google.com>
2023-06-05 13:36:34 -0700

    license

--- core/lib/deprecation-description.js
+++ core/lib/deprecation-description.js
@@ -1,3 +1,9 @@
+/**
+ * @license Copyright 2023 The Lighthouse Authors. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
 import * as i18n from '../lib/i18n/i18n.js';
 import {DEPRECATIONS_METADATA, UIStrings as DeprecationUIStrings} from './deprecations-strings.js';
 
@@ -24,7 +30,7 @@ const deprecationsStr_ = i18n.createIcuMessageFn(import.meta.url, DeprecationUIS
 /**
  * @param {LH.Crdp.Audits.DeprecationIssueDetails} issueDetails
  */
-function getDescription(issueDetails) {
+function getIssueDetailDescription(issueDetails) {
   let message;
   const type = /** @type {keyof DEPRECATIONS_METADATA} */ (issueDetails.type);
   const maybeEnglishMessage = DeprecationUIStrings[type];
@@ -60,6 +66,6 @@ function getDescription(issueDetails) {
 }
 
 export {
-  getDescription as getIssueDetailDescription,
+  getIssueDetailDescription,
   UIStrings,
 };
